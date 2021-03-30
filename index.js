1b9fc666739b57cf0d383f950457489c145c6c6a
Tran Ngoc Quoc <tnquoc1998@gmail.com>
2021-03-30 10:53:35 +0700

    simple nodejs server

---
+++ index.js
@@ -0,0 +1,11 @@
+const app = require("express")();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`App listens at port ${PORT}`);
+})
